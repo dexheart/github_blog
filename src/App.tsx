e0a99d9342f56f4035429d86c4e1d36cee83c237
Tiago <tiago.linhares.dh@gmail.com>
2023-03-08 18:10:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from 'styled-components'
+
+export function App() {
+  return (
+    <ThemeProvider theme={default}>
+      <h1>ola imundo</h1>
+    </ThemeProvider>
+  )
+}
